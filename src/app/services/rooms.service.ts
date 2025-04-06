22277dcff5f0ba49c02a04af054fb3d014b03e0e
K19G <kfirgraidi@gmail.com>
2025-04-06 16:56:37 +0300

    Added Messages page

--- src/app/services/rooms.service.ts
+++ src/app/services/rooms.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { Room } from '../models/room';
 import { Observable, of } from 'rxjs';
 import { RoomType } from '../models/room-type';
+import { Building } from '../models/building';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class RoomsService {
+  buildings: Building[];
   rooms: Room[];
 
   constructor() {
@@ -16,13 +18,25 @@ export class RoomsService {
       new Room(2, "200", RoomType.ComputerClass, "D", 2, 40, 1, false),
       new Room(3, "100", RoomType.Lab, "B", 1, 50, 0, true),
     ];
+    this.buildings = [
+      new Building(1, "B"),
+      new Building(2, "D"),
+      new Building(3, "M"),
+    ];
   }
 
   getAllRooms(): Observable<Room[]> {
     return of(this.rooms);
   }
-
-  removeRoom() {
+  addRoom(): void {
     
   }
+  removeRoom(): void {
+    
+  }
+
+  getAllBuildings(): Observable<Building[]> {
+    return of(this.buildings);
+  }
 }
+
